31582df52e38dbef367594f296e893ed92c45081
Abdul Wahab Khan <awkhan313@gmail.com>
2024-10-02 11:20:33 +0500

    refactoring some folders

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_template/app_prod.dart';
+import 'package:flutter_template/flavors/app_prod.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
